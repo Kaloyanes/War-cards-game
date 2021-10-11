8b40a0d1c9a2119ff46ebb5bbd781923500e567d
Kaloyan Stoyanov <kaloyangfx@gmail.com>
2021-10-11 16:03:17 +0300

    Add project files.

---
+++ War cards/Card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Markup;
+
+namespace War_cards
+{
+    public class Card
+    {
+        private List<int> values = new List<int>()
+        {
+            2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14
+        };
+
+        public int GetCard()
+        {
+            var rnd = new Random();
+            var random = rnd.Next(2, 14);
+            return values[random];
+        }
+    }
+}
---
+++ War cards/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+
+namespace War_cards
+{
+    internal class Program
+    {
+        private static int cards = 52;
+        private static int player1 = 0;
+        private static int player2 = 0;
+
+        private static Queue player1cards
+
+        private static int player1wins = 0;
+        private static int player2wins = 0;
+
+        static void Main(string[] args)
+        {
+            var card = new Card();
+            for (int i = 0; i < cards; i++)
+            {
+                if (i % 2 == 0)
+                {
+
+                }
+            }
+        }
+    }
+}
