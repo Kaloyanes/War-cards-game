6a893005d5cc1e83218de5d61060d8d65a0408f4
Kaloyan Stoyanov <kaloyangfx@gmail.com>
2021-10-13 18:52:55 +0300

    Refactoring

--- War cards/Program.cs
+++ War cards/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 
 namespace War_cards
 {
@@ -15,16 +16,6 @@ namespace War_cards
         private static int player2wins = 0;
 
         private static Random rnd = new Random();
-        static public void Shuffle(int[] deck)
-        {
-            for (int n = deck.Length - 1; n > 0; --n)
-            {
-                int k = rnd.Next(n + 1);
-                int temp = deck[n];
-                deck[n] = deck[k];
-                deck[k] = temp;
-            }
-        }
 
         static void Main(string[] args)
         {
@@ -45,191 +36,164 @@ namespace War_cards
             }
 
             Console.WriteLine("Cards are distributed");
-            Pause();
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
             Console.Clear();
+
+
             int rounds = 0;
 
-            while (player1cards.Count != 0 || player2cards.Count != 0)
+            while (true)
             {
                 if (player1cards.Count == 0 || player2cards.Count == 0) break;
                 rounds++;
                 PlayGame(rounds);
             }
-            
+
+            Console.WriteLine($"Player 1 round wins: {player1wins} | Player 2 round wins: {player2wins}");
+        }
+        static public void Shuffle(int[] deck)
+        {
+            for (int n = deck.Length - 1; n > 0; --n)
+            {
+                int k = rnd.Next(n + 1);
+                int temp = deck[n];
+                deck[n] = deck[k];
+                deck[k] = temp;
+            }
         }
 
         public static void PlayGame(int rounds)
         {
             if (player1cards.Count == 0 || player2cards.Count == 0) return;
-            string card;
-            switch (player1cards.Peek())
-            {
-                case 11:
-                    card = "J";
-                    break;
-            
-                case 12:
-                    card = "Q";
-                    break;
-            
-                case 13:
-                    card = "K";
-                    break;
-            
-                case 14:
-                    card = "A";
-                    break;
-
-                default:
-                    card = player1cards.Peek().ToString();
-                    break;
-            }
-
-            if (player1cards.Count == 0 || player2cards.Count == 0) return;
-            string card2;
-            switch (player2cards.Peek())
-            {
-                case 11:
-                    card2 = "J";
-                    break;
-
-                case 12:
-                    card2 = "Q";
-                    break;
-
-                case 13:
-                    card2 = "K";
-                    break;
-
-                case 14:
-                    card2 = "A";
-                    break;
-
-                default:
-                    card2 = player2cards.Peek().ToString();
-                    break;
-
-
-            }
+            string card = Convert(player1cards);
+            string card2 = Convert(player2cards);
 
             Console.WriteLine($"Player 1 card:{card}  |  Player 2 card: {card2}");
 
-            string winner = "";
-            if (player1cards.Peek() > player2cards.Peek())
-            {
-                winner = "Player 1";
-                player1wins++;
-                player1cards.Enqueue(player2cards.Peek());
-                player1cards.Dequeue();
-                player2cards.Dequeue();
-            }
-            else if (player1cards.Peek() < player2cards.Peek())
-            {
-                winner = "Player 2";
-                player2wins++;
-                player2cards.Enqueue(player1cards.Peek());
-                player2cards.Dequeue();
-                player1cards.Dequeue();
-            }
-            else if (player1cards.Peek() == player2cards.Peek())
-            {
-                player2cards.Dequeue();
-                player1cards.Dequeue();
-                War(rounds);
-                return;
-            }
-
+            var winner = Check(rounds);
             Console.WriteLine($"{winner} Wins round #{rounds}!");
             Console.WriteLine();
         }
 
-        public static void War(int rounds)
+        public static string Check(int rounds)
         {
             string winner = "";
-            if (player1cards.Count == 0 || player2cards.Count == 0) return;
 
-            var player1card = "";
-            var player2card = "";
-
-            var player1num = player1cards.Peek();
-            var player2num = player2cards.Peek();
-            switch (player1num)
-            {
-                case 11:
-                    player1card = "J";
-                    break;
-
-                case 12:
-                    player1card = "Q";
-                    break;
-
-                case 13:
-                    player1card = "K";
-                    break;
-
-                case 14:
-                    player1card = "A";
-                    break;
-
-                default:
-                    player1card = player1cards.Peek().ToString();
-                    break;
-            }
-
-            switch (player2num)
-            {
-                case 11:
-                    player2card = "J";
-                    break;
-
-                case 12:
-                    player2card = "Q";
-                    break;
-
-                case 13:
-                    player2card = "K";
-                    break;
-
-                case 14:
-                    player2card = "A";
-                    break;
-
-                default:
-                    player2card = player2cards.Peek().ToString();
-                    break;
-            }
+            int player1num = player1cards.Peek();
+            int player2num = player2cards.Peek();
             if (player1num > player2num)
             {
                 winner = "Player 1";
                 player1wins++;
+                // The won cards are going back to the queue to be used again later
                 player1cards.Enqueue(player2num);
                 player1cards.Enqueue(player1num);
+
+                // Removing the top cards so the cards don't repeat
+                player1cards.Dequeue();
                 player2cards.Dequeue();
             }
             else if (player1num < player2num)
             {
                 winner = "Player 2";
                 player2wins++;
+
+                // The won cards are going back to the queue to be used again later
                 player2cards.Enqueue(player1num);
                 player2cards.Enqueue(player2num);
+
+                // Removing the top cards so the cards don't repeat
                 player1cards.Dequeue();
+                player2cards.Dequeue();
+            }
+            else
+            {
+                player1cards.Dequeue();
+                player2cards.Dequeue();
+
+                // Going into a recursive function until a winner is decided
+                return War(rounds);
+            }
+
+            return winner;
+        }
+
+        public static string War(int rounds)
+        {
+            if (player1cards.Count == 0 && player1cards.Count == 0) return "";
+
+            string winner = "";
+
+            int player1num = player1cards.Peek();
+            int player2num = player2cards.Peek();
+
+            if (player1num > player2num)
+            {
+                winner = "Player 1";
+                player1wins++;
+
+                // The won cards are going back to the queue to be used again later
+                player1cards.Enqueue(player2num);
+                player1cards.Enqueue(player1num);
+                
+                // Removing the top cards so the cards don't repeat
+                player1cards.Dequeue();
+                player2cards.Dequeue();
+            }
+            else if (player1num < player2num)
+            {
+                winner = "Player 2";
+                player2wins++;
+
+                // The won cards are going back to the queue to be used again later
+                player2cards.Enqueue(player1num);
+                player2cards.Enqueue(player2num);
+
+                // Removing the top cards so the cards don't repeat
+                player1cards.Dequeue();
+                player2cards.Dequeue();
             }
             else
             {
                 player2cards.Dequeue();
                 player1cards.Dequeue();
-                War(rounds);
+
+                // Going into a recursive function until a winner is decided
+                return War(rounds);
             }
 
-            Console.WriteLine($"Player 1 card:{player1card}  |  Player 2 card: {player2card}");
-            Console.WriteLine($"{winner} Wins round #{rounds}!");
-            Console.WriteLine();
+            return winner;
         }
 
-        public static void Pause()
+        // Converts card to symbols
+        public static string Convert(Queue<int> player)
         {
-            Console.WriteLine("Press any key to continue...");
-            Console.ReadKey();
+            string playercard = "";
+            switch (player.Peek())
+            {
+                case 11:
+                    playercard = "J";
+                    break;
+
+                case 12:
+                    playercard = "Q";
+                    break;
+
+                case 13:
+                    playercard = "K";
+                    break;
+
+                case 14:
+                    playercard = "A";
+                    break;
+
+                default:
+                    playercard = player.Peek().ToString();
+                    break;
+            }
+
+            return playercard;
         }
     }
 }
